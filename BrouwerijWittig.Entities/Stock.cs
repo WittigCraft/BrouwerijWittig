0532d85a9f527d7cdec5c79a940a042e8a6c5246
WittigCraft <chris_w86@hotmail.com>
2026-03-08 23:16:59 +0100

    Remove Snacks property and ProductId assignment from Stock
    Removed List<Snacks> property and its initialization in Stock class. Also removed ProductId assignment from the constructor. Beers list initialization was removed as well. Other constructor logic and Products list initialization remain unchanged.

--- BrouwerijWittig.Entities/Stock.cs
+++ BrouwerijWittig.Entities/Stock.cs
@@ -13,7 +13,6 @@ namespace BrouwerijWittig.Entities
         public bool FactoryOrdered { get; set; }
         public DateTime BackInStock { get; set; }
         public List<Product> Products { get; set; }
-        public List<Snacks> Snacks { get; set; }
         
 
         public Stock()
@@ -24,7 +23,7 @@ namespace BrouwerijWittig.Entities
 
         public Stock(Guid productId, string productName, bool inStock, int amountInStock, Category category, bool factoryOrdered, DateTime backInStock)
         {
-               ProductId = productId;
+               
                ProductName = productName;
                InStock = inStock;
                AmountInStock = amountInStock;
@@ -34,8 +33,6 @@ namespace BrouwerijWittig.Entities
                if (AmountInStock > 0) InStock = true;
                InStock = InStock;
                Products = new List<Product>();
-               Beers = new List<Beer>();
-               Snacks = new List<Snacks>();
         }
 
         
